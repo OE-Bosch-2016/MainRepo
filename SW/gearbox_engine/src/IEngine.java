741067e016ff92ccb76ca85cb20c349fd07c7d57
Guttin László <guttin.laszlo@gmail.com>
2016-02-25 19:24:02 +0100

    #247 Updated version.

--- SW/gearbox_engine/src/IEngine.java
+++ SW/gearbox_engine/src/IEngine.java
@@ -5,5 +5,10 @@ package egineandgearbox;
  * @author Laci
  */
 public interface IEngine {
+
     void operateEngine(int gearState, boolean gasPedal);
+
+    double getRpm();
+
+    double getTorque();
 }
