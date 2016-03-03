0282cf4255084446be71c867b49fac7f4304641f
f-viktor <ferter.viktor@gmail.com>
2016-03-03 14:52:56 +0100

    Added files via upload
    Base Wheels class requiring interface.

---
+++ SW/wheel/src/Wheels.java
@@ -0,0 +1,55 @@
+package wheels;
+
+class Wheels {
+//author: Ferter Viktor
+//Last modified: Sipos Pal
+	
+	
+	double speed; 						   //given in km/h 
+	double direction; 					   //given in degree 0-360
+	
+	//adjust these by testing
+    static double turningAdjustment = 0.2;      //proportion to simulate turning realistically
+	static double accelerationAdjustment = 0.2; //proportion to convert torque to acceleration
+	static double brakeAdjustment = 0.2;		//proportion to turn brake pedal to deceleration
+	static double natureBrake = 0.2;			//deceleration of environment
+    
+	public Wheels()
+	{
+	speed=0;
+	direction=0;
+	}
+	
+	public Wheels(double direction)
+	{
+		speed=0;
+		this.direction=direction;
+	}
+	
+	public Wheels(double direction, double speed)
+	{
+		this.speed=speed;
+		this.direction=direction;
+	}
+    
+	
+	public void calcOnTick(double driverWheel,double gasPedal, double brakePedal)
+	{
+		calcDirection(driverWheel);    //calculates first because new direction is effected by last speed
+		calcSpeed(gasPedal,brakePedal);
+	}
+	
+	private void calcDirection(double driverWheel) 
+	{
+		double phiDirection=driverWheel*speed*turningAdjustment;
+		direction += phiDirection;
+	}
+	
+	private void calcSpeed(double torque, double brakePedal)
+	{
+		speed+= accelerationAdjustment*torque
+				-brakeAdjustment*brakePedal
+				-natureBrake;
+	}
+
+}
