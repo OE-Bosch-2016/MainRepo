5dfc6182546693c7370beb8d671cc5915e251c1c
József Mezei <jozsimezei@gmail.com>
2016-03-05 00:41:56 +0100

    #238 added mileage UI

--- SW/visualisation/src/src/Top.java
+++ SW/visualisation/src/src/Top.java
@@ -1,7 +1,11 @@
-import HMI.IMileage;
 import HMI.Mileage;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.dial.*;
+import org.jfree.data.general.DefaultValueDataset;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by haxxi on 2016.03.01..
@@ -12,9 +16,18 @@ public class Top extends JFrame {
     private JTextArea hmi_mileage_text_area;
     private JSlider test_slider;
     private JPanel rootPanel;
+    private JPanel ui_panel;
+
 
     // HMI elements
     private Mileage mileage;
+
+    // Delete ??
+    private static final int DISPLAY_MAX = 220;
+    private final DefaultValueDataset dataset = new DefaultValueDataset();
+    private final DefaultValueDataset displayDataset = new DefaultValueDataset();
+    private final JFrame frame = new JFrame();
+
 
     public Top() {
         init();
@@ -29,6 +42,24 @@ public class Top extends JFrame {
         mileage = new Mileage();
         mileage.setMileAgeListener(mileAgeListener);
 
+
+        //****
+        frame.setPreferredSize(new Dimension(300, 300));
+        frame.add(buildDialPlot(0, DISPLAY_MAX, 20));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        setValue(50);
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                frame.setVisible(true);
+            }
+        });
+        //*****
+
         // Test
         test_slider.addChangeListener(e -> {
            int value = test_slider.getValue();
@@ -36,6 +67,39 @@ public class Top extends JFrame {
         });
     }
 
+    private void setValue(int value) {
+        dataset.setValue(value);
+        displayDataset.setValue(Math.min(DISPLAY_MAX, value));
+    }
+
+    private ChartPanel buildDialPlot(int minimumValue, int maximumValue,
+                                     int majorTickGap) {
+
+        DialPlot plot = new DialPlot();
+        plot.setDataset(0, dataset);
+        plot.setDataset(1, displayDataset);
+
+        plot.setDialFrame(new StandardDialFrame());
+
+        // value indicator uses the real data set
+        plot.addLayer(new DialValueIndicator(0));
+
+        // needle uses constrained data set
+        plot.addLayer(new DialPointer.Pointer(1));
+
+        StandardDialScale scale = new StandardDialScale(minimumValue, maximumValue,
+                -120, -300, majorTickGap, majorTickGap - 1);
+        scale.setTickRadius(0.88);
+        scale.setTickLabelOffset(0.20);
+        plot.addScale(0, scale);
+
+        return new ChartPanel(new JFreeChart(plot));
+    }
+
+
     // Listener --------------------------------------------------------------------------------------------------------
-    private Mileage.OnMileAgeListener mileAgeListener = mile -> hmi_mileage_text_area.setText(String.valueOf(mile) + Mileage.UNIT);
+    private Mileage.OnMileAgeListener mileAgeListener = mile -> {
+        hmi_mileage_text_area.setText(String.valueOf(mile) + Mileage.UNIT);
+        setValue((int) mile);
+    };
 }
