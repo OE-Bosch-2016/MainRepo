94ff4fc4a4079ab66a9713d3b6f957a43ff3ae0c
József Mezei <jozsimezei@gmail.com>
2016-03-04 22:19:01 +0100

    #238 added ui elements

--- SW/visualisation/src/src/Top.java
+++ SW/visualisation/src/src/Top.java
@@ -1,9 +1,20 @@
+import HMI.IMileage;
+import HMI.Mileage;
+
 import javax.swing.*;
 
 /**
  * Created by haxxi on 2016.03.01..
  */
 public class Top extends JFrame {
+
+    // UI elements
+    private JTextArea hmi_mileage_text_area;
+    private JSlider test_slider;
+    private JPanel rootPanel;
+
+    // HMI elements
+    private Mileage mileage;
 
     public Top() {
         init();
@@ -12,6 +23,19 @@ public class Top extends JFrame {
     private void init() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+        setContentPane(rootPanel);
+        pack();
+
+        mileage = new Mileage();
+        mileage.setMileAgeListener(mileAgeListener);
+
+        // Test
+        test_slider.addChangeListener(e -> {
+           int value = test_slider.getValue();
+            mileage.mileage(value);
+        });
     }
 
+    // Listener --------------------------------------------------------------------------------------------------------
+    private Mileage.OnMileAgeListener mileAgeListener = mile -> hmi_mileage_text_area.setText(String.valueOf(mile) + Mileage.UNIT);
 }
