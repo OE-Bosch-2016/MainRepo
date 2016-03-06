2259c9e3e94a5181a79c9da762c66fc100cd75e6
peterhidvegi <peter.hidvegi92@gmail.com>
2016-03-06 22:46:18 +0100

    #240 interface and enum

---
+++ SW/visualisation/src/src/main/java/Light/ILightable.java
@@ -0,0 +1,12 @@
+package Light;
+
+/**
+ * Created by Perec on 2016.03.06..
+ */
+public interface ILightable {
+
+    boolean isTurned();
+
+    TypeOfLight getTypeOfLamp();
+
+}
---
+++ SW/visualisation/src/src/main/java/Light/TypeOfLight.java
@@ -0,0 +1,9 @@
+package Light;
+
+/**
+ * Created by Perec on 2016.03.06..
+ */
+public enum TypeOfLight {
+
+    SideDirectionIndicator,LowBeam,ParkingLight,HighBeam,DayTimeRunningLight,FogLamp,CorneringLight,FrontDirectionIndicator,StopLamp,BackUpLamp,RearDirectionIndicator,TailLight,RearFogLamp
+}
