73b55b736c644f9b9e97546f907e548b9c033926
József Mezei <jozsimezei@gmail.com>
2016-03-13 01:38:12 +0100

    added config + Scalr,

---
+++ SW/visualisation/src/src/main/java/Utils/Config.java
@@ -0,0 +1,13 @@
+package Utils;
+
+/**
+ * Created by haxxi on 2016.03.13..
+ */
+public class Config {
+
+    public static int imageSizeX = 400;
+    public static int imageSizeY = 400;
+    public static String pathMap1 = "../resources/maps/road_1.png";
+    public static String pathMap2 = "../resources/maps/road_2.png";
+
+}
