6150d3196442ba73c931fba2f0e4040f97b538db
f-viktor <ferter.viktor@gmail.com>
2016-03-18 14:31:12 +0100

    Create Camera.java
    base class architecture and guessed object architectures

---
+++ SW/wheel/src/Camera.java
@@ -0,0 +1,42 @@
+package camera;
+
+public class Camera implements ICamera {
+	
+	Object closestSign; 						    //given in the object itself
+	double laneDistance;							//meters or pixels define which one! 
+	Object laneType;								//given in degree 0-360
+	
+	
+	
+	
+  
+	
+	private void getClosestSign(Object[] visibleObjects, Object car) 
+	{
+		//set closest sign
+		double min=999999;
+		int minIndex=-1;
+		int distX=0;
+		int distY=0;
+		for(int i=0;i<visibleObjects.length;i++)
+		{
+		distX = (visibleObjects[i].xPos- car.xPos)*(visibleObjects[i].xPos- car.xPos);
+		distY =	(visibleObjects[i].yPos- car.yPos)*(visibleObjects[i].yPos- car.yPos);
+			if(Math.sqrt(distX+distY)<min)
+			{
+				min=Math.sqrt(distX+distY)<min;
+				minIndex=i;
+			}
+		}
+		closestSign=visibleObjects[minIndex];
+	}
+	
+	private void getLaneDistance(Object car, Object road)
+	{
+		//set distance from lane
+		//set lane type
+	}
+	
+	//+interface implementációk
+	
+}
