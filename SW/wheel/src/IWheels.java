0b795ef36c265e5691d214df3017c89d9d0fb39c
f-viktor <ferter.viktor@gmail.com>
2016-03-18 15:01:46 +0100

    Update IWheels.java
    adjusted base class function interface to change

--- SW/wheel/src/IWheels.java
+++ SW/wheel/src/IWheels.java
@@ -3,5 +3,5 @@ package wheels;
 public interface IWheels {
 	double Direction();		//0-360 degree
 	double Speed();
-	void calcOnTick(double driverWheel,double gasPedal, double brakePedal);
+	void calcOnTick(double driverWheel,double torque ,double RPM, double brakePedal);
 }
