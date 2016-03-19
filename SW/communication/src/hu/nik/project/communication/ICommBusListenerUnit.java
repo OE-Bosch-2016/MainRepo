bc984a058eaceae0107365cad99f2921e2a1f629
zhodvogner@gmail.com <zhodvogner@gmail.com>
2016-03-19 20:24:55 +0100

    Reorganisation of communication modul and time-out management in CommBus methods

---
+++ SW/communication/src/hu/nik/project/communication/ICommBusListenerUnit.java
@@ -0,0 +1,18 @@
+package hu.nik.project.communication;
+
+import jdk.internal.util.xml.impl.Input;
+
+import java.io.InputStream;
+import java.util.stream.Stream;
+
+/**
+ * Created by zhodvogner on 2016.03.19.
+ *
+ * Interface of communication bus listener.
+ * This unit can connect to CommBus, will get CommBusEvents and can read data from the bus.
+ */
+public interface ICommBusListenerUnit {
+
+    public void commBusEvent( InputStream inputStream );
+
+}
