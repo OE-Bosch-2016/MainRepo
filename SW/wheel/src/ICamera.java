556486a6c82d9637e1c20c8fb621e8ebf404908f
Apor <Apor@users.noreply.github.com>
2016-03-19 10:04:54 +0100

    Update ICamera.java

--- SW/wheel/src/ICamera.java
+++ SW/wheel/src/ICamera.java
@@ -1,6 +1,10 @@
-package wheel;
+package camera;
 
 public interface ICamera {
 	int posX();		//or double
 	int posY();
+	
+	Object closestSign(); 	//given in the object itself
+	double laneDistance();	//meters or pixels define which one! 
+	Object laneType();	//given in degree 0-360
 }
