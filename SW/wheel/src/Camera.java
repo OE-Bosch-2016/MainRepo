2706b12704ea4d2e7ff9e7b937422cb3edc1e2db
f-viktor <ferter.viktor@gmail.com>
2016-03-19 01:02:01 +0100

    replaced placeholder names
    Adjusted placeholder function and class names and imported used classes from the actual enviroment repo

--- SW/wheel/src/Camera.java
+++ SW/wheel/src/Camera.java
@@ -1,39 +1,59 @@
 package camera;
 
+///class definitions are found in Team1 repo at OE-Bosch-2016-Team1/MainRepo/blob/master/SW/environment/src/hu/nik/project/environment/
+import hu.nik.project.environment.SceneObject;	 //for understanding SceneObjects
+import hu.nik.project.environment.ScenePoint;	 //for understanding ScenePoints
+import hu.nik.project.environment.Sign;			//for understanding Signs
+import hu.nik.project.environment.DirectionSign;
+import hu.nik.project.environment.ParkingSing;	
+import hu.nik.project.environment.PrioritySign;	
+import hu.nik.project.environment.SpeedSign;
+
 public class Camera implements ICamera {
 	
-	Object closestSign; 						    //given in the object itself
-	double laneDistance;							//meters or pixels define which one! 
-	Object laneType;								//given in degree 0-360
+	Object closestSign; 	//given in the object itself
+	double laneDistance;	//meters or pixels define which one! 
+	Object laneType;	//given in degree 0-360
 	
-	private void getClosestSign(Object[] visibleObjects, Object car) 
+	private void calcClosestSign(SceneObject[] visibleObjects, SceneObject car) 
 	{
 		//set closest sign
-		double min=999999;
+		double min=999999;	//irrationally high number for minimum selection
 		int minIndex=-1;
 		int distX=0;
 		int distY=0;
-		for(int i=0;i<visibleObjects.length;i++)
+		for(int i=0;i<visibleObjects.length;i++) 					//calculate (x1-x2)^2*(y1-y2)^2 for each
 		{
-		distX = (visibleObjects[i].xPos- car.xPos)*(visibleObjects[i].xPos- car.xPos);
-		distY =	(visibleObjects[i].yPos- car.yPos)*(visibleObjects[i].yPos- car.yPos);
-			if(Math.sqrt(distX+distY)<min)
+		distX = (visibleObjects[i].getBasePosition.getX- car.getBasePosition.getX)*
+		(visibleObjects[i].getBasePosition.getX- car.getBasePosition.getX);
+		
+		distY =	(visibleObjects[i].getBasePosition.getY- car.getBasePosition.getY)*
+		(visibleObjects[i].getBasePosition.getY- car.getBasePosition.getY);
+		
+			if(Math.sqrt(distX+distY)<min && 
+				(
+				visibleObjects[i].getObjectType ==DirectionSign ||
+				visibleObjects[i].getObjectType ==ParkingSign ||
+				visibleObjects[i].getObjectType ==PrioritySign ||
+				visibleObjects[i].getObjectType ==SpeedSign
+				))
 			{
 				min=Math.sqrt(distX+distY);
 				minIndex=i;
 			}
 		}
-		if(minIndex=-1){
+		
+		if(minIndex==-1)
+		{
 			closestSign=null;
 		}
 		else
 		{
 			closestSign=visibleObjects[minIndex];
 		}
-			
 	}
 	
-	private void getLaneDistance(Object car, Object road)
+	private void calcLaneDistance(Object car, Object road)
 	{
 		//set distance from lane
 		//set lane type
