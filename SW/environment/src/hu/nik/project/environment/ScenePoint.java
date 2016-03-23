9db62d4a693fc6f73f686f206935b487dce5d787
Hodvogner Zoltán <hodvognerz@navsky.hu>
2016-03-23 13:48:07 +0100

    fix of ScenePoint.isVisibleFromObserver - managing extreme input values

--- SW/environment/src/hu/nik/project/environment/ScenePoint.java
+++ SW/environment/src/hu/nik/project/environment/ScenePoint.java
@@ -48,19 +48,21 @@ public class ScenePoint {
     }
 
     public static boolean isVisibleFromObserver(ScenePoint observerBase, int observerRotation, int viewAngle, int viewDistance, ScenePoint point ) {
-        if(observerBase==null || point==null) return false;
+        if (observerBase==null || point==null) return false;
+        if (viewAngle<=0) return false;
+        if (viewDistance<0) return false; // viewDistance equals zero is meaning the infinite
         double SPx = point.getX() - observerBase.getX();
         double SPy = point.getY() - observerBase.getY();
         if ((viewDistance > 0) && (Math.sqrt( (SPx*SPx) + (SPy*SPy) ) > viewDistance)) return false; // not visible because out of view-distance
         double viewAngleOfSP;
-        if (SPx == 0) {
+        if (SPx == 0) { // special cases
             if (SPy == 0) return false; // observer and object on same place
             if (SPy > 0)
                 viewAngleOfSP = 270;     // point is above the observer
             else
                 viewAngleOfSP = 90;    // point is below the observer
         }
-        else {
+        else {  // normal, but four different cases (4 regions of the coordinate-system)
             if (SPx > 0 && SPy < 0) // in t1 (+,-)
                 viewAngleOfSP = Math.toDegrees(Math.atan(-SPy / SPx));
             else if (SPx < 0 && SPy < 0) // in t2 (-,-)
@@ -71,10 +73,30 @@ public class ScenePoint {
                 viewAngleOfSP = 360 - Math.toDegrees(Math.atan(SPy / SPx));
         }
         if (viewAngleOfSP == 360) viewAngleOfSP = 0;
-        double halfViewAngle = viewAngle / 2;
+        // viewAngleOfSP between 0 and 360 degrees
+
+        double halfViewAngle = (double)viewAngle / 2;
         if (viewAngle > 360) halfViewAngle = (viewAngle % 360) / 2;
-        if ( viewAngleOfSP > (observerRotation + halfViewAngle) ) return false; // not visible because above max-angle
-        return viewAngleOfSP >= (observerRotation - halfViewAngle);
+        // +/- half-viewangle => HVA
+
+        double rotation = observerRotation;
+        if (rotation > 360) rotation = rotation % 360;
+        if (rotation > 180) rotation = rotation - 360;  // --> -180 < rotation < +180
+
+        double minAngle = rotation - halfViewAngle;
+        if (minAngle < 0) minAngle = minAngle + 360;  // rotation - HVA
+
+        double maxAngle = rotation + halfViewAngle;
+        if (maxAngle < 0) maxAngle = maxAngle + 360;  // rotation + HVA
+
+        // unfortunately (in case of extremly high viewangle) minAngle might be the greater value
+        // in this case must decrease minAngle and return to -180,+180 interval
+        if (minAngle > maxAngle) {
+            if (minAngle > 180) minAngle = minAngle - 360;
+        }
+
+        if ( viewAngleOfSP > maxAngle ) return false; // not visible because above max-angle
+        return viewAngleOfSP >= minAngle;
     }
 
 }
