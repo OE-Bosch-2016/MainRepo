bc98b00796ad0b26989a171ec79dcd67a584fa58
József Mezei <jozsimezei@gmail.com>
2016-03-31 00:37:26 +0200

    updated main class

--- SW/visualisation/src/src/main/java/ParkingPilot/PPMain.java
+++ SW/visualisation/src/src/main/java/ParkingPilot/PPMain.java
@@ -1,7 +1,31 @@
 package ParkingPilot;
 
+import ParkingPilot.Manager.PPManager;
+
+import java.awt.*;
+
 /**
  * Created by haxxi on 2016.03.30..
  */
 public class PPMain {
+
+    private PPManager manager;
+
+    public PPMain() {
+
+        manager = PPManager.newInstance();
+        manager.setSenderListener(parkingListener);
+    }
+
+    public void parkingPilotActivate(){
+        manager.sendPPData(4, 160f, new Point(0,1));
+    }
+
+    // Listener --------------------------------------------------------------------------------------------------------
+    private PPManager.ParkinPilotListener parkingListener = new PPManager.ParkinPilotListener() {
+        public void onDataChanged() {
+
+        }
+    };
+
 }
