2a76fc725ddb2147bf370224e5d57d5220022a26
Guttin László <guttin.laszlo@gmail.com>
2016-04-01 18:22:31 +0200

    #339 Create basic ACC...

---
+++ SW/acc/src/ACC/src/acc/ACC.java
@@ -0,0 +1,38 @@
+package acc;
+
+/**
+ *
+ * @author Laci
+ */
+public class ACC implements IACC {
+
+    private final int minFollowingDistance = 20; //in meters
+    private final int maxSpeed = 180; //in km/h
+
+    private int pedal;
+
+    @Override
+    public int getPedal() {
+        return pedal;
+    }
+
+    public ACC() {
+        pedal = 0;
+    }
+
+    @Override
+    public void PedalState(int wheelStateInDegrees, boolean ACCState, int currentSpeed, int nearestObstacleDistance) {
+        if (ACCState) {//ACC is on
+            if ((currentSpeed <= maxSpeed - Math.abs(wheelStateInDegrees * 2)) && (nearestObstacleDistance > minFollowingDistance)) {//Should we accelerate?
+                //throttling
+                pedal = 1;
+            } else if ((nearestObstacleDistance <= minFollowingDistance) || (currentSpeed > maxSpeed - Math.abs(wheelStateInDegrees * 2))) {//Should we brake?
+                //breaking
+                pedal = -1;
+            } else {
+                //do nothing
+                pedal = 0;
+            }
+        }
+    }
+}
---
+++ SW/acc/src/ACC/src/acc/IACC.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acc;
+
+/**
+ *
+ * @author Laci
+ */
+public interface IACC {
+    int getPedal();
+    void PedalState(int wheelStateInDegrees, boolean ACCState, int currentSpeed, int nearestObstacleDistance);
+}
