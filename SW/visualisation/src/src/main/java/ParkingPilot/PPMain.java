3f91894a87d40271de092cb3e03422be83258f29
József Mezei <jozsimezei@gmail.com>
2016-04-02 14:10:22 +0200

    updated main

--- SW/visualisation/src/src/main/java/ParkingPilot/PPMain.java
+++ SW/visualisation/src/src/main/java/ParkingPilot/PPMain.java
@@ -1,9 +1,8 @@
 package ParkingPilot;
 
 import ParkingPilot.Manager.PPManager;
-
-import java.util.ArrayList;
-import java.util.List;
+import ParkingPilot.Model.Parking;
+import ParkingPilot.Util.Calculator;
 import java.awt.*;
 
 /**
@@ -12,27 +11,28 @@ import java.awt.*;
 public class PPMain {
 
     private PPManager manager;
+    private Calculator calculator;
 
     public PPMain() {
-
         manager = PPManager.newInstance();
         manager.setSenderListener(parkingListener);
+        calculator = new Calculator();
     }
 
     public void parkingPilotActivate(){
-        manager.sendPPData(4, 160f, new Point(0,1));
-    }
-
-    private List<Point> calculateOptimalPath(){
-        List<Point> path = new ArrayList<Point>();
-
-        return path;
+        manager.sendPPData(300, 180f, new Point(100,50));
     }
 
     // Listener --------------------------------------------------------------------------------------------------------
-    private PPManager.ParkinPilotListener parkingListener = new PPManager.ParkinPilotListener() {
+    private PPManager.ParkingPilotListener parkingListener = new PPManager.ParkingPilotListener() {
         public void onDataChanged() {
-
+            Parking parking = manager.getParking();
+            Point[] car = new Point[4];
+            car[0] = new Point(300, 150);
+            car[1] = new Point(300, 50);
+            car[2] = new Point(100, 150);
+            car[3] = new Point(100, 50);
+            calculator.parking(car, parking.getCar1(), parking.getCar2(), parking.getEdgeOfStreet(), calculator.MODIFY_HORIZONTAL);
         }
     };
 
