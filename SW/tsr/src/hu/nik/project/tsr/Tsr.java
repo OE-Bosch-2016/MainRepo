5e4e2dd91497c8b2e1b5bcc034630b37ebfcc27b
CyberZeroHun <ambrus.attila@stud.uni-obuda.hu>
2016-04-07 01:56:54 +0200

    Tsr alapok

---
+++ SW/tsr/src/hu/nik/project/tsr/Tsr.java
@@ -0,0 +1,8 @@
+package hu.nik.project.tsr;
+
+/**
+ * Created by CyberZero on 2016. 04. 07..
+ */
+public class Tsr {
+
+}
---
+++ SW/tsr/test/hu/nik/project/tsr/TsrTest.java
@@ -0,0 +1,12 @@
+package hu.nik.project.tsr;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Created by CyberZero on 2016. 04. 07..
+ */
+public class TsrTest {
+
+}
