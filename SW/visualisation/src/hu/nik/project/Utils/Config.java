9ff9403691bddde19bba49ffee0162817fbc9d7b
József Mezei <jozsimezei@gmail.com>
2016-04-10 21:10:17 +0200

    refactored the project

--- SW/visualisation/src/hu/nik/project/Utils/Config.java
+++ SW/visualisation/src/hu/nik/project/Utils/Config.java
@@ -7,13 +7,13 @@ public class Config {
 
     public static int imageSizeX = 800;
     public static int imageSizeY = 600;
-    public static String pathMap1 = "hu/nik/resources/maps/road_1.png";
-    public static String pathMap2 = "hu/nik/resources/maps/road_2.png";
+    public static String pathMap1 = "src/hu/nik/resources/maps/road_1.png";
+    public static String pathMap2 = "src/hu/nik/resources/maps/road_2.png";
 
-    public static String pathCar = "hu/nik/resources/entities/Car2.png";
+    public static String pathCar = "src/hu/nik/resources/entities/Car2.png";
     public static int carImageSizeX = 24;
     public static int carImageSizeY = 24;
 
-    public static String pathSteeringWheel = "hu/nik/resources/steeringwheel.png";
-    public static String getPathSteeringWheel = "main/resources/steeringwheel.png";
+    public static String pathSteeringWheel = "src/hu/nik/resources/steeringwheel.png";
+    public static String getPathSteeringWheel = "src/hu/nik/resources/steeringwheel.png";
 }
