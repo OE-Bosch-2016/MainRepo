35173b62bab1e24d03fad0b932c4c8f5c6bf8ae5
f-viktor <ferter.viktor@gmail.com>
2016-04-10 17:19:20 +0200

    removed posx,y no point to have them

--- SW/wheel/src/hu/nik/project/Camera/ICamera.java
+++ SW/wheel/src/hu/nik/project/Camera/ICamera.java
@@ -3,9 +3,7 @@ package hu.nik.project.camera;
 import hu.nik.project.environment.objects.SceneObject;
 
 public interface ICamera {
-	int posX();		//or double
-	int posY();
-	
+
 	public SceneObject getClosestSign(); 	//given in the object itself
 	public double getLaneDistance();	//meters or pixels define which one! 
 	public SceneObject getLaneType();	//given in degree 0-360
