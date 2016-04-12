67a41cf5335414ebc43fe46477594334a16b769f
Apor <Apor@users.noreply.github.com>
2016-04-12 14:23:13 +0200

    Added files via upload

---
+++ SW/wheel/src/hu/nik/project/Camera/CameraMessagePackage.java
@@ -0,0 +1,9 @@
+package hu.nik.project.camera;
+
+import hu.nik.project.environment.objects.SceneObject;
+
+public class CameraMessagePackage {
+	public SceneObject ClosestSign;
+	public double LaneDistance;
+	public SceneObject LaneType;
+}
