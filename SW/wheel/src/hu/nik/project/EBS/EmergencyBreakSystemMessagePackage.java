a8a06b5fc74da0dc5a13dfda5d28f84022f89035
Apor <Apor@users.noreply.github.com>
2016-04-12 14:28:00 +0200

    Added files via upload

---
+++ SW/wheel/src/hu/nik/project/EBS/EmergencyBreakSystemMessagePackage.java
@@ -0,0 +1,10 @@
+package hu.nik.project.ebs;
+
+import java.util.ArrayList;
+import java.util.List;
+import hu.nik.project.environment.objects.SceneObject;
+
+public class EmergencyBreakSystemMessagePackage {
+	public SceneObject car;
+	public List<SceneObject> jaywalkers;
+}
