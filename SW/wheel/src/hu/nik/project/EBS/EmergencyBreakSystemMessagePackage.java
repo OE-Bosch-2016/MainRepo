e477c708b80fd2b9329f74fe3ea35a22d9304be1
Apor <Apor@users.noreply.github.com>
2016-04-13 16:06:37 +0200

    Update EmergencyBreakSystemMessagePackage.java

--- SW/wheel/src/hu/nik/project/EBS/EmergencyBreakSystemMessagePackage.java
+++ SW/wheel/src/hu/nik/project/EBS/EmergencyBreakSystemMessagePackage.java
@@ -6,4 +6,9 @@ import hu.nik.project.environment.objects.SceneObject;
 
 public class EmergencyBreakSystemMessagePackage {
 	public Boolean breaking;		//to break or not to break. that is the question
+	
+	public void EmergencyBreakSystemMessagePackage(SceneObject car, List<SceneObject> jaywalk){
+		this.car = car;
+		this.jaywalkers = jaywalk;
+	}
 }
