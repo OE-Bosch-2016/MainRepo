0a981c4c8779d29b1fb26373cc3841c4b0f6b84a
Apor <Apor@users.noreply.github.com>
2016-04-13 17:50:38 +0200

    Update CameraMessagePackage.java

--- SW/wheel/src/hu/nik/project/camera/CameraMessagePackage.java
+++ SW/wheel/src/hu/nik/project/camera/CameraMessagePackage.java
@@ -1,14 +1,14 @@
 package hu.nik.project.camera;
 
 import hu.nik.project.environment.objects.SceneObject;
-import 
+import java.util.Arrays;
 
 public class CameraMessagePackage {
 	public SceneObject ClosestSign;
 	public double LaneDistance;
 	public SceneObject LaneType;
 	
-	public void CameraMessagePackage(SceneObject closestSign, double laneDistance, SceneObject laneType, List<SceneObject>visibleObjects){
+	public void CameraMessagePackage(SceneObject closestSign, double laneDistance, SceneObject laneType, Arrays[] visibleObjects){
 		this.ClosestSign = closestsign;
 		this.LaneDistance = laned;
 		this.LaneType = laneType
