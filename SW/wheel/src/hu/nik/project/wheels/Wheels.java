cd5413d285dfd3ac87587c6f70db666d686b1cfa
f-viktor <kacsavacsora@gmail.com>
2016-04-13 14:24:10 +0200

    fuckmylife

--- SW/wheel/src/hu/nik/project/wheels/Wheels.java
+++ SW/wheel/src/hu/nik/project/wheels/Wheels.java
@@ -1,47 +1,69 @@
+
+
 package hu.nik.project.wheels;
 
-public class Wheels implements IWheels {
-//author: Ferter Viktor
-//Last modified: Sipos Pal
-//Last Last modified: Apor
-	
-	
-	double speed; 						   //given in km/h 
-	double direction; 					   //given in degree 0-360
-	
+import hu.nik.project.communication;
+
+public class Wheels implements IWheels, ICommBusDevice {
+
+	//imput buffers
+	private int EngineRPM;
+	private int EngineTorque;
+	private int HMIWheel;
+
 	//adjust these by testing
-    	static double turningAdjustment = 0.01;      //proportion to simulate turning realistically
-    	static double framerate = 24;
-	static double accelerationAdjustment = 0.2; //proportion to convert torque to acceleration
-	static double brakeAdjustment = 0.2;		//proportion to turn brake pedal to deceleration
-	static double natureBrake = 0.02;			//deceleration of environment
-    
-	public Wheels()
-	{
-	speed=0;
-	direction=0;
+	static private double turningAdjustment = 0.01;      //proportion to simulate turning realistically
+	static private double framerate = 24;
+	static private double accelerationAdjustment = 0.2; //proportion to convert torque to acceleration
+	static private double brakeAdjustment = 0.2;		//proportion to turn brake pedal to deceleration
+	static private double natureBrake = 0.02;			//deceleration of environment
+
+	//output buffers
+	private double speed; 						   //given in km/h
+	private double direction; 					   //given in degree 0-360
+
+	@Override
+	public void commBusDataArrived() {
+		if (commBusConnector.getDataType() == EnginePacket || commBusConnector.getDataType() == HMIPacket ) {
+
+			//dataType = commBusConnector.getDataType();
+			if (commBusConnector.getDataType() == EnginePacket) {
+				try {
+					EngineRPM = ((EnginePacket)commBusConnector.receive()).RPM;
+					EngineTorque = ((EnginePacket)commBusConnector.receive()).Torque;
+
+				} catch (CommBusException e) {
+					stringData = e.getMessage();
+				}
+			}
+
+			if (commBusConnector.getDataType() == HMIPacket) {
+				try {
+					HMIWheel = ((HMIPacket)commBusConnector.receive()).DriverWheelAngle;
+
+				} catch (CommBusException e) {
+					stringData = e.getMessage();
+				}
+			}
+		}
 	}
-	
-	public Wheels(double direction)
+
+	public Wheels(CommBus commBus, CommBusConnectorType commBusConnectorType)
 	{
+		commBusConnector = commBus.createConnector(this, commBusConnectorType);
+
 		speed=0;
-		this.direction=direction;
+		direction=0;
 	}
-	
-	public Wheels(double direction, double speed)
-	{
-		this.speed=speed;
-		this.direction=direction;
-	}
-    
-	
+
+
 	public void calcOnTick(double driverWheel,double torque ,double RPM, double brakePedal)
 	{
 		calcDirection(driverWheel);    //calculates first because new direction is effected by last speed
 		calcSpeed(torque, brakePedal, RPM);
 	}
-	
-	private void calcDirection(double driverWheel) 
+
+	private void calcDirection(double driverWheel)
 	{
 		double phiDirection=driverWheel*speed*turningAdjustment/framerate;
 		direction += phiDirection;
@@ -51,34 +73,34 @@ public class Wheels implements IWheels {
 			direction=360+direction;
 		}
 	}
-	
-	private void calcSpeed(double torque, double brakePedal, double RPM)
-	{
+
+	private void calcSpeed(double torque, double brakePedal, double RPM) {
 		/*
 		Power (kW) = Torque (N.m) x Speed (RPM) /0.0095488
 		c=0.3
 		D=1.25
 		A=3
 		v = (2*P/(c*D*A))^(1/3)
-		natureBrake=0.3*1.25*3=1.125; 
+		natureBrake=0.3*1.25*3=1.125;
 		*/
-		
-		speed = Math.pow((2*(torque*RPM/0.0095488)/(natureBrake)), new Double("0.3333333"));
-		if(torque >= 0)
-		{
+
+		speed = Math.pow((2 * (torque * RPM / 0.0095488) / (natureBrake)), new Double("0.3333333"));
+		if (torque >= 0) {
 			speed -= brakeAdjustment * brakePedal;
-		}
-		else
-		{
+		} else {
 			speed += brakeAdjustment * brakePedal;
 		}
 	}
-	
+
+	public void SendToCom() {
+		new WheelPacket(speed,direction);
+		while(!commBusConnector.send(WheelPacket)); //is this gonna work even?
+	}
 	//Interface metodusok
 	public double Direction(){
 		return direction;
 	}
-	
+
 	public double Speed(){
 		return speed;
 	}
