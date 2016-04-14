6a812d0797d05b6f94a5fa00bddec67699d33eb7
f-viktor <kacsavacsora@gmail.com>
2016-04-14 19:07:15 +0200

    test file creation for wheels

---
+++ SW/wheel/test/hu/nik/project/camera/CameraTest.java
@@ -0,0 +1,48 @@
+package hu.nik.project.camera;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lmpala on 2016.04.14..
+ */
+public class CameraTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void commBusDataArrived() throws Exception {
+
+    }
+
+    @Test
+    public void sendToCom() throws Exception {
+
+    }
+
+    @Test
+    public void getClosestSign() throws Exception {
+
+    }
+
+    @Test
+    public void getLaneDistance() throws Exception {
+
+    }
+
+    @Test
+    public void getLaneType() throws Exception {
+
+    }
+
+}
---
+++ SW/wheel/test/hu/nik/project/wheels/WheelsTest.java
@@ -0,0 +1,36 @@
+package hu.nik.project.wheels;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lmpala on 2016.04.14..
+ */
+public class WheelsTest {
+    @Test
+    public void commBusDataArrived() throws Exception {
+
+    }
+
+    @Test
+    public void calcOnTick() throws Exception {
+
+    }
+
+    @Test
+    public void sendToCom() throws Exception {
+
+    }
+
+    @Test
+    public void direction() throws Exception {
+
+    }
+
+    @Test
+    public void speed() throws Exception {
+
+    }
+
+}
