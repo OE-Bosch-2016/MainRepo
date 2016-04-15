2f0ef05df8d67672e3ff8ef7918a2b4433fc29fd
József Mezei <jozsimezei@gmail.com>
2016-04-15 22:01:54 +0200

    added paths to config

--- SW/visualisation/src/hu/nik/project/utils/Config.java
+++ SW/visualisation/src/hu/nik/project/utils/Config.java
@@ -7,12 +7,12 @@ public class Config {
 
     public static int imageSizeX = 800;
     public static int imageSizeY = 600;
-    public static String pathMap1 = "src/hu/nik/resources/maps/road_1.png";
-    public static String pathMap2 = "src/hu/nik/resources/maps/road_2.png";
+    public static String pathMap1 = "visualisation/src/hu/nik/resources/maps/road_1.png";
+    public static String pathMap2 = "visualisation/src/hu/nik/resources/maps/road_2.png";
 
-    public static String pathCar = "src/hu/nik/resources/entities/car.png";
+    public static String pathCar = "visualisation/src/hu/nik/resources/entities/car.png";
     public static int carImageSizeX = 24;
     public static int carImageSizeY = 24;
 
-    public static String pathSteeringWheel = "src/hu/nik/resources/steeringwheel.png";
+    public static String pathSteeringWheel = "visualisation/src/hu/nik/resources/steeringwheel.png";
 }
