62ea5fd945a0c90d02a8e74e3d16606dddaadc99
KommerszUnicum <csomorr@gmail.com>
2016-04-17 00:21:03 +0200

    - Modified framework module declaration - Adding engine and gearbox module dependency to framework

--- SW/framework/src/hu/nik/project/framework/BoschCar.java
+++ SW/framework/src/hu/nik/project/framework/BoschCar.java
@@ -1,16 +1,16 @@
 package hu.nik.project.framework;
 
 import hu.nik.project.communication.CommBusConnectorType;
-import hu.nik.project.engineandgearbox.Gearbox;
+import hu.nik.project.gearbox.Gearbox;
 import hu.nik.project.environment.ScenePoint;
 import hu.nik.project.environment.objects.Car;
 import hu.nik.project.environment.objects.SceneObjectException;
 import hu.nik.project.tsr.Tsr;
 import hu.nik.project.wheels.Wheels;
-import hun.nik.project.acc.ACC;
+import hu.nik.project.acc.ACC;
 import hu.nik.project.communication.CommBus;
-import hu.nik.project.engineandgearbox.Engine;
-import ultrasonicsensor.Ultrasonicsensor;
+import hu.nik.project.engine.Engine;
+import hu.nik.project.ultrasonicsensor.Ultrasonicsensor;
 
 /**
  * Created by Róbert on 2016.04.10..
@@ -31,10 +31,10 @@ public class BoschCar extends Car {
     public BoschCar(ScenePoint basePosition, int rotation) throws SceneObjectException {
         super(basePosition, rotation);
 
-        acc = new ACC();
         commBus = new CommBus();
-        engine = new Engine(0);
-        gearbox = new Gearbox(0);
+        acc = new ACC(commBus, CommBusConnectorType.SenderReceiver);
+        engine = new Engine(commBus, CommBusConnectorType.SenderReceiver);
+        gearbox = new Gearbox(0, commBus, CommBusConnectorType.SenderReceiver);
         tsr = new Tsr(commBus, CommBusConnectorType.SenderReceiver);
         wheels = new Wheels(commBus, CommBusConnectorType.SenderReceiver);
     }
