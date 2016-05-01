38cf946b49cb4fe4a58489cac8e5eedc3501be5a
József Mezei <jozsimezei@gmail.com>
2016-05-01 15:29:37 +0200

    added ScenePoint

--- SW/visualisation/src/hu/nik/project/utils/Transformation.java
+++ SW/visualisation/src/hu/nik/project/utils/Transformation.java
@@ -1,20 +1,22 @@
 package hu.nik.project.utils;
 
+import hu.nik.project.environment.ScenePoint;
+
 /**
  * Created by haxxi on 2016.05.01..
  */
 public class Transformation {
 
-    public static Vector2D transformFromVector2D(Vector2D coordinate){
+    public static ScenePoint transformFromVector2D(Vector2D coordinate){
         float rate[] = scaleRate();
 
-        return new Vector2D(coordinate.get_coordinateX() / rate[0], coordinate.get_coordinateY() / rate[1]);
+        return new ScenePoint((int)(coordinate.get_coordinateX() / rate[0]), (int)(coordinate.get_coordinateY() / rate[1]));
     }
 
-    public static Vector2D transformToVector2D(Vector2D coordinate){
+    public static Vector2D transformToVector2D(ScenePoint coordinate){
         float rate[] = scaleRate();
 
-        return new Vector2D(coordinate.get_coordinateX() * rate[0], coordinate.get_coordinateY() * rate[1]);
+        return new Vector2D(coordinate.getX() * rate[0], coordinate.getY() * rate[1]);
     }
 
     private static float[] scaleRate(){
