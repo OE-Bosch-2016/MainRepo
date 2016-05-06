a72b35e65fdedcf3f3ade67b0d3d16399928df9a
andrastyukasz <tyuki1393@freemail.hu>
2016-05-06 20:25:47 +0200

    Updated Sonar

--- SW/ultrasonicsensor/src/hu/nik/project/ultrasonicsensor/UltrasonicModul.java
+++ SW/ultrasonicsensor/src/hu/nik/project/ultrasonicsensor/UltrasonicModul.java
@@ -28,23 +28,25 @@ public class UltrasonicModul implements ICommBusDevice {
     private int startAngle = 0;
     private CommBusConnector commBusConnector;
     private Scene scene;
+    private Car car;
+
     public double[] getClosestDistance() {
         return closestDistance;
     }
-    
+
     public Sonar[] getSonars() {
         return sonars;
     }
-    
 
     public UltrasonicModul(CommBus commBus, CommBusConnectorType commBusConnectorType, Car car, Scene scene) {
         commBusConnector = commBus.createConnector(this, commBusConnectorType);
         closestDistance = new double[8];
         sonars = new Sonar[8];
+        this.car = car;
         currPosition = car.getBasePosition();
         this.scene = scene;
         for (int i = 0; i < sonars.length; i++) {
-            sonars[i] = new Sonar(fov, startAngle, currPosition);
+            sonars[i] = new Sonar(fov, startAngle, car.getBasePosition());
             startAngle += fov;
         }
     }
@@ -52,44 +54,33 @@ public class UltrasonicModul implements ICommBusDevice {
     @Override
     public void commBusDataArrived() {
     }
-
-//    public void SendToBus() {
-//        UltrasonicMessagePackage umsg = new UltrasonicMessagePackage(closestDistance);
-//        boolean sent = false;
-//        while (!sent) {
-//            try {
-//                if (commBusConnector.send(umsg)) {
-//                    sent = true;
-//                }
-//            } catch (CommBusException e) {
-//                break;
-//            }
-//        }
-//    }
-    public void SendToBus() throws CommBusException {
-        UltrasonicMessagePackage umsg = new UltrasonicMessagePackage(closestDistance);
-        commBusConnector.send(umsg);
-    }
     
-    public void getNearestObjectDistances(ScenePoint currPos, int rotation) throws CommBusException
-    {
+    public void doWork() {
+        getNearestObjectDistances(car.getBasePosition(), car.getRotation());
+        UltrasonicMessagePackage umsg = new UltrasonicMessagePackage(closestDistance);
+        try {
+            if (commBusConnector.send(umsg)) {
+            }
+        } catch (CommBusException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void getNearestObjectDistances(ScenePoint currPos, int rotation) {
         startAngle = 0;
         for (int i = 0; i < sonars.length; i++) {
             sonars[i] = new Sonar(fov, startAngle, currPos);
             startAngle += fov;
         }
-        
+
         for (int i = 0; i < sonars.length; i++) {
             Sonar son = sonars[i];
-            son.setCurrPosition(currPos);
             ScenePoint newSonarPos = ScenePoint.rotatePointAroundPoint(currPos, son.getSonarPos(), rotation);
-//            System.out.println(son.getSonarPos()+"; "+newSonarPos+ " ;"+son.getStartAngle()+"; "+(son.getStartAngle()+rotation));
-            closestDistance[i] = son.getNearestObjectDistance(scene.getVisibleSceneObjects(newSonarPos,(son.getStartAngle()+rotation), (int) son.getFov(), son.getSonarViewDis()), newSonarPos);
-            
+            System.out.println(son.getSonarPos() + "; " + newSonarPos + " ;" + son.getStartAngle() + "; " + (son.getStartAngle() + rotation));
+            closestDistance[i] = son.getNearestObjectDistance(scene.getVisibleSceneObjects(newSonarPos, (son.getStartAngle() + rotation), (int) son.getFov(), son.getSonarViewDis()), newSonarPos);
+
         }
-        
-        SendToBus();
+
     }
 
 }
